a11d121eb9b5b0608f807054d0b8d858e8d0c2c6
unknown <ishitagupta07.igg@gmail.com>
2022-12-17 11:10:18 +0530

    Hello World Program

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/helloworld')
+
+def helloworld():
+    return "Hello , Welcome you will start creating your first CICD Pipeline"
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
